c3d569bd0a80d91fe923b2a28d070008b7e489e6
Dipankar Barua <barua.dipankar04@gmail.com>
2019-03-20 14:26:57 +0100

    responsive mobile & Widescreen

--- js/main.js
+++ js/main.js
@@ -18,6 +18,16 @@ function initMap() {
 }
 
 
+// Sticky menu background
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+
 
 //smooth scrolling
 
